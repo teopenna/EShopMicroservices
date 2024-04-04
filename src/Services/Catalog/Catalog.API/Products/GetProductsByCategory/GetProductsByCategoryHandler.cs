7f7fdbe89702a99e34cbbcfec6a6bf106c25637e
Matteo Penna <penna.matteo@gmail.com>
2024-04-04 08:48:10 +0200

    Add Get Product By Id and Get Products By Category endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,20 @@
+namespace Catalog.API.Products.GetProductsByCategory;
+
+public record GetProductsByCategoryQuery(string Category) : IQuery<GetProductsByCategoryResult>;
+public record GetProductsByCategoryResult(IEnumerable<Product> Products);
+
+public class GetProductsByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductsByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductsByCategoryQuery, GetProductsByCategoryResult>
+{
+    public async Task<GetProductsByCategoryResult> Handle(GetProductsByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsByCategoryQueryHandler.Handle called with {@Query}", query);
+        
+        var products = await session
+            .Query<Product>()
+            .Where(x => x.Categories.Contains(query.Category))
+            .ToListAsync(cancellationToken);
+        
+        return new GetProductsByCategoryResult(products);
+    }
+}
